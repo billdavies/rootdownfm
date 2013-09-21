0b9563cb42a5310b8eae02a1b34de915d0e7cc7d
billdavie5 <bill@grdinternet.com>
2013-09-21 22:51:37 +0000

    Init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rootdownfm::Application.config.secret_key_base = '46efe9c0c99f8975ecf9fdadc2aa99d67e35afce632b8f91d70c03068f39b55bd9aef8aca219be61e15ebe38549cf929eca357ec4d11a72f2480570558d03e4d'
